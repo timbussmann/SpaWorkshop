4db7656fcad383204eb3312ae0e18c3020b1ef03
timbussmann <notthemail@youarelooking.for>
2013-05-31 16:24:58 +0200

    Added basic StoryRepository

---
+++ SpaWorkshop/Models/StoryRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace SpaWorkshop.Models
+{
+    using System.Collections.Generic;
+
+    public class StoryRepository : IStoryRepository
+    {
+        public StoryRepository()
+        {
+            this.UserStories = new[]
+                {
+                    new UserStory() {Title = "Demo"},
+                };
+        }
+
+        public IEnumerable<UserStory> UserStories { get; private set; }
+    }
+}
