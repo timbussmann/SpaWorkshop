7d13465981bce878e0c566e9dfe0157a012ac227
timbussmann <notthemail@youarelooking.for>
2013-06-17 10:38:30 +0200

    Added ngView with route configs

--- SpaWorkshop/App/appModule.js
+++ SpaWorkshop/App/appModule.js
@@ -1,1 +1,9 @@
-﻿var app = angular.module('scrumboard', []);
+﻿var app = angular.module('scrumboard', [], function($routeProvider, $locationProvider) {
+    $routeProvider.when('/scrumboard', {
+        templateUrl: '/App/views/scrumboardView.html',
+        controller: 'scrumboardController'
+    });
+    $routeProvider.otherwise({ redirectTo: '/scrumboard' });
+    
+    $locationProvider.html5Mode(true);
+});
