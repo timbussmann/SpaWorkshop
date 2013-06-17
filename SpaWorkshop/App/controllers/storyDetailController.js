2bcdea350953906830ae8849d074ec42c5879e32
timbussmann <notthemail@youarelooking.for>
2013-06-17 21:16:23 +0200

    Added bindings for detail view

--- SpaWorkshop/App/controllers/storyDetailController.js
+++ SpaWorkshop/App/controllers/storyDetailController.js
@@ -1,4 +1,7 @@
-﻿app.controller('storyDetailController', function($scope, $http, $routeParams) {
+﻿app.controller('storyDetailController', function ($scope, $http, $routeParams) {
+
+    $scope.states = ['Sprint Backlog', 'Work In Progress', 'To Verify', 'Done'];
+
     $http.get('/api/story/' + $routeParams.storyId).success(function(story) {
         $scope.story = story;
     });
