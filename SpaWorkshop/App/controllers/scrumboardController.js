221aa62da63f79ba8fe37160628ea49a170be29d
timbussmann <notthemail@youarelooking.for>
2013-06-17 23:30:03 +0200

    Added basic chat

--- SpaWorkshop/App/controllers/scrumboardController.js
+++ SpaWorkshop/App/controllers/scrumboardController.js
@@ -3,4 +3,20 @@
     $http.get('/api/stories').success(function (stories) {
         $scope.stories = stories;
     });
+
+    $scope.messages = '';
+    var hub = $.connection.chatHub;
+    hub.client.addMessage = function(message) {
+        $scope.messages += 'Someone else:' + message + '\n';
+        $scope.$apply();
+    };
+    
+    $.connection.hub.start();
+
+    $scope.sendMessage = function () {
+        var message = $scope.chatMessage;
+        $scope.messages += 'You:' + message + '\n';
+        hub.server.sendMessage(message);
+        $scope.chatMessage = '';
+    };
 });
