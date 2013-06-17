f6f9c4efac2f928a67b19734b35b5b81b4b84c9e
timbussmann <notthemail@youarelooking.for>
2013-06-17 17:00:37 +0200

    show story details

--- SpaWorkshop/App/appModule.js
+++ SpaWorkshop/App/appModule.js
@@ -3,6 +3,10 @@
         templateUrl: '/App/views/scrumboardView.html',
         controller: 'scrumboardController'
     });
+    $routeProvider.when('/details/:storyId', {
+        templateUrl: '/App/views/storyDetailView.html',
+        controller: 'storyDetailController'
+    });
     $routeProvider.otherwise({ redirectTo: '/scrumboard' });
     
     $locationProvider.html5Mode(true);
---
+++ SpaWorkshop/App/controllers/storyDetailController.js
@@ -0,0 +1,5 @@
+﻿app.controller('storyDetailController', function($scope, $http, $routeParams) {
+    $http.get('/api/story/' + $routeParams.storyId).success(function(story) {
+        $scope.story = story;
+    });
+});
