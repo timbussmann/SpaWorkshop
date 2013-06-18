0086bc0c5e812db841b9e7d8e2c7cee018ea7b5d
timbussmann <notthemail@youarelooking.for>
2013-06-19 00:19:34 +0200

    removed chat views und service

--- SpaWorkshop/App/appModule.js
+++ SpaWorkshop/App/appModule.js
@@ -1,7 +1,8 @@
 ﻿var app = angular.module('scrumboard', [], function($routeProvider, $locationProvider) {
 
     $routeProvider.when('/scrumboard', {
-        templateUrl: '/App/views/scrumboardView.html'
+        templateUrl: '/App/views/scrumboardView.html',
+        controller: 'scrumboardController'
     });
     $routeProvider.when('/details/:storyId', {
         templateUrl: '/App/views/storyDetailView.html',
@@ -10,32 +11,4 @@
     $routeProvider.otherwise({ redirectTo: '/scrumboard' });
 
     $locationProvider.html5Mode(true);
-});
-
-app.service('chatService', function($rootScope) {
-
-    var self = this;
-    this.messages = sessionStorage.getItem('chat') || '';
-
-    var hub = $.connection.chatHub;
-    if (hub){
-        hub.client.addMessage = function(message) {
-            addMessage('Someone else: ' + message);
-
-            $rootScope.$apply(function() {
-                $rootScope.$broadcast('MessageReceived');
-            });
-        };
-        $.connection.hub.start();
-    }
-
-    this.sendMessage = function(message) {
-        addMessage('You: ' + message);
-        hub.server.sendMessage(message);
-    };
-
-    function addMessage(message) {
-        self.messages += message + '\n';
-        sessionStorage.setItem('chat', self.messages);
-    }
 });
