18b6254439499c45efaf3e948604410feec26fa5
timbussmann <notthemail@youarelooking.for>
2013-06-19 12:31:00 +0200

    removed all unnecessary elements

--- SpaWorkshop/App/appModule.js
+++ SpaWorkshop/App/appModule.js
@@ -1,13 +1,1 @@
-﻿var app = angular.module('scrumboard', [], function($routeProvider, $locationProvider) {
-
-    $routeProvider.when('/scrumboard', {
-        templateUrl: '/App/views/scrumboardView.html',
-        controller: 'scrumboardController'
-    });
-    
-    // add additional routes here
-    
-    $routeProvider.otherwise({ redirectTo: '/scrumboard' });
-
-    $locationProvider.html5Mode(true);
-});
+﻿var app = angular.module('scrumboard', []);
--- SpaWorkshop/App/controllers.js
+++ SpaWorkshop/App/controllers.js
@@ -4,8 +4,3 @@
         $scope.stories = stories;
     });
 });
-
-app.controller('storyDetailController', function ($scope, $http, $routeParams, $location) {
-
-    $scope.states = ['Sprint Backlog', 'Work In Progress', 'To Verify', 'Done'];
-});
